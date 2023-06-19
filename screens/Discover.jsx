3f58ccca67a0a7af1e2003098b71f4f0ead19230
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-06-19 19:17:00 +0100

    worked on header for discover page

--- screens/Discover.jsx
+++ screens/Discover.jsx
@@ -1,11 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { Avatar } from '../assets';
 
 const Discover = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
+
+
   return (
-    <View>
-      <Text>Discover</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white relative">
+      <View className="flex-row items-center justify-between px-8">
+        <View>
+          <Text className="text-[#0B646B] text-[40px] font-bold">Discover</Text>
+          <Text className="text-[#5272B3] text-[36px]">the beauty today</Text>
+        </View>
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center shadow-md">
+          <Image
+            source={Avatar}
+            className="w-full h-full rounded-md object-cover" 
+          />
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
