c4aabe0747eaafbd4b7c48c4cfa8120202cef8f6
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-06-19 18:20:00 +0100

    worked on the first section

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,11 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native'
 
 const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-white">
+      {/* First section */}
+      <View className="flex-row px-6 mt-8 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#00BCC9] text-3xl font-semibold">
+            Go
+          </Text>
+        </View>
+        <Text className="text-[#2A2B4B] text-3xl font-semibold">
+          Travel
+        </Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
