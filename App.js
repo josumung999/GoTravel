c4524d0e7c03d3afbd1caa3823a165a1cb6efa8b
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-06-19 18:01:23 +0100

    added the first screen

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <NavigationContainer>
-        
-      </NavigationContainer>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Accueil" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
