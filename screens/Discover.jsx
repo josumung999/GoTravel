4ad2492f0b8fe6cb296767ecdf55927d3fd39e33
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2023-06-20 09:16:02 +0100

    added empty and loading state

--- screens/Discover.jsx
+++ screens/Discover.jsx
@@ -1,7 +1,7 @@
-import { View, Text, SafeAreaView, Image, ScrollView, TouchableOpacity } from 'react-native'
+import { View, Text, SafeAreaView, Image, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native'
 import React, { useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
-import { Attractions, Avatar, Hotels, Restaurants } from '../assets';
+import { Attractions, Avatar, Hotels, NotFound, Restaurants } from '../assets';
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 import MenuContainer from '../components/MenuContainer';
 import { FontAwesome } from '@expo/vector-icons';
@@ -11,6 +11,8 @@ const Discover = () => {
   const navigation = useNavigation();
 
   const [type, setType] = useState('restaurants');
+  const [isLoading, setIsLoading] = useState(false);
+  const [mainData, setMainData] = useState([]);
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -51,52 +53,74 @@ const Discover = () => {
       </View>
 
       {/* Menu container */}
-      <ScrollView>
-        <View className="flex-row items-center justify-between px-8 mt-8">
-          <MenuContainer 
-            key="hotel"
-            title="Hotels"
-            imageSrc={Hotels}
-            type={type}
-            setType={setType}
-          />
-          <MenuContainer 
-            key="attractions"
-            title="Attractions"
-            imageSrc={Attractions}
-            type={type}
-            setType={setType}
-          />
-          <MenuContainer 
-            key="restaurants"
-            title="Restaurants"
-            imageSrc={Restaurants}
-            type={type}
-            setType={setType}
-          />
+      {isLoading ? (
+        <View className="flex-1 items-center justify-center">
+          <ActivityIndicator size="large" color="#0B646B" />
         </View>
-
-        <View>
-          <View className="px-4 flex-row items-center justify-between mt-8" >
-            <Text className="text-[#2C7379] text-[28px] font-bold ">Top Tips</Text>
-            <TouchableOpacity className="flex-row items-center justify-center space-x-2">
-              <Text className="text-[#A0C4C7] text-[20px] font-bold">Explore</Text>
-              <FontAwesome name='long-arrow-right' color="#A0C4C7" />
-            </TouchableOpacity>
+      ) : (
+        <ScrollView>
+          <View className="flex-row items-center justify-between px-8 mt-8">
+            <MenuContainer 
+              key="hotel"
+              title="Hotels"
+              imageSrc={Hotels}
+              type={type}
+              setType={setType}
+            />
+            <MenuContainer 
+              key="attractions"
+              title="Attractions"
+              imageSrc={Attractions}
+              type={type}
+              setType={setType}
+            />
+            <MenuContainer 
+              key="restaurants"
+              title="Restaurants"
+              imageSrc={Restaurants}
+              type={type}
+              setType={setType}
+            />
           </View>
 
-          <View className="px-4 mt-8 flex-row items-center justify-evenly flex-wrap">
-            {[1, 2, 3, 4, 5, 6].map((item) => (
-              <ItemCardContainer 
-                key={item}
-                imageSrc="https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg"
-                title="Mon Beguin"
-                location="Kinshasa"
-              />
-            ))}
+          <View>
+            <View className="px-4 flex-row items-center justify-between mt-8" >
+              <Text className="text-[#2C7379] text-[28px] font-bold ">Top Tips</Text>
+              <TouchableOpacity className="flex-row items-center justify-center space-x-2">
+                <Text className="text-[#A0C4C7] text-[20px] font-bold">Explore</Text>
+                <FontAwesome name='long-arrow-right' color="#A0C4C7" />
+              </TouchableOpacity>
+            </View>
+
+            <View className="px-4 mt-8 flex-row items-center justify-evenly flex-wrap">
+              {mainData?.length > 0 ? (
+                <>
+                  {[1, 2, 3, 4, 5, 6].map((item) => (
+                    <ItemCardContainer 
+                      key={item}
+                      imageSrc="https://cdn.pixabay.com/photo/2015/10/30/12/22/eat-1014025_1280.jpg"
+                      title="Mon Beguin"
+                      location="Kinshasa"
+                    />
+                  ))}
+                </>
+              ) : (
+                <>
+                  <View className="w-full h-[400px] items-center space-y-8 justify-center">
+                    <Image
+                      source={NotFound}
+                      className=" w-32 h-32 object-cover"
+                    />
+                    <Text className="text-2xl text-[#428288] font-semibold">
+                      Opps...No Data Found
+                    </Text>
+                  </View>
+                </>
+              )}
+            </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      )}
     </SafeAreaView>
   )
 }
